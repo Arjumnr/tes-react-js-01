158a087adb36378d13b115989ce7b9801b79716f
arjum nur ramadhan <arjumnurramadhan@gmail.com>
2022-03-08 23:39:48 +0800

    route

--- src/index.js
+++ src/index.js
@@ -2,15 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Main from './pages/home/main';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/ma" element={<Main />} />
+      <Route path="/" element={<App />} />
+      
+    </Routes>
+  </BrowserRouter>,
+ document.getElementById('root')
+  
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ src/pages/home/main/index.js
@@ -0,0 +1,8 @@
+function main(){
+    return (
+        <div>
+            <h1>React Js nih bOSS</h1>
+        </div>
+    );
+}
+ export default main();
